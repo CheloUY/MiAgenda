ffde20c491b51c292eff8e540547d31c44000d56
CheloUY <mlopezlage@gmail.com>
2020-10-06 00:19:36 -0300

    Initial commit

---
+++ app/src/main/java/com/marcelolopezlage/miagenda/confirmarDatos.java
@@ -0,0 +1,14 @@
+package com.marcelolopezlage.miagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class confirmarDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirmar_datos);
+    }
+}
